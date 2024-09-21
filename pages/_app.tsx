448eec7326bdac59e1a7dfca4419c65cde2bc521
Sabil49 <md.sabeel10@gmail.com>
2024-09-21 22:16:04 +0530

    blog graphql application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,20 @@
-import '../styles/tailwind.css';
-import Layout from '../components/Layout';
-import type { AppProps } from 'next/app';
+import '../styles/tailwind.css'
+import { UserProvider } from '@auth0/nextjs-auth0/client'
+import Layout from '../components/Layout'
+import { ApolloProvider } from '@apollo/client'
+import type { AppProps } from 'next/app'
+import apolloClient from '../lib/apollo'
 
-function MyApp({ Component, pageProps }:AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <UserProvider>
+            <ApolloProvider client={apolloClient}>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </ApolloProvider>
+        </UserProvider>
+    )
 }
 
-export default MyApp;
+export default MyApp
---
+++ pages/api/auth/hook.ts
@@ -0,0 +1,29 @@
+﻿import prisma from '../../../lib/prisma';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { email, secret } = req.body;
+    // 1
+    if (req.method !== 'POST') {
+        console.log('Method not allowed')
+        return res.status(403).json({ message: 'Method not allowed' });
+    }
+    // 2
+    if (secret !== process.env.AUTH0_HOOK_SECRET) {
+        console.log('You must provide the secret 🤫')
+        return res.status(403).json({ message: `You must provide the secret 🤫` });
+    }
+    // 3
+    if (email) {
+        console.log('create')
+        // 4
+        await prisma.user.create({
+            data: { email },
+        });
+        return res.status(200).json({
+            message: `User with email: ${email} has been created successfully!`,
+        });
+    }
+};
+
+export default handler;
